662cd8b530a2f5285e207dc99326f158fa99e635
Rohan Aggarwal <rohanaggarwal090204@gmail.com>
2025-07-20 04:27:02 +0530

    Signup logic with password encrypted

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const bodyParser=require('body-parser');
 const {PORT}= require('./config/serverConfig');
-
+const ApiRoutes = require('./routes/index.js');
 const app= express();
 
 const prepareAndStartServer=()=>{
+
+    app.use(bodyParser.json());
+
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',ApiRoutes);
 
     app.listen(PORT,()=>{
         console.log(`Server Started on Port: ${PORT}`);
---
+++ src/routes/v1/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const UserController = require('../../controllers/user-controller');
+// const {AuthRequestValidator} = require('../../middlewares/index')
+
+const router = express.Router();
+
+router.post('/signup',
+    UserController.create
+);
+
+// router.post('/signin',
+//     AuthRequestValidator.validateUserAuth,
+//     UserController.signIn
+// );
+
+// router.get('/isauthenticated',
+//         UserController.isAuthenticated
+// );
+
+// router.get(
+//     '/isAdmin',
+//     AuthRequestValidator.validateIsAdminRequest,
+//     UserController.isAdmin
+// );
+
+// router.get('/user/:userId', UserController.getDetails);
+
+module.exports = router;
