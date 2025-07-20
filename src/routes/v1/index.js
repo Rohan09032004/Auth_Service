1228452d9e189cb1c395fdc4f45c05401c8d85d0
Rohan Aggarwal <rohanaggarwal090204@gmail.com>
2025-07-20 17:35:54 +0530

    Added email and password valudation for incoming entries

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const UserController = require('../../controllers/user-controller');
-// const {AuthRequestValidator} = require('../../middlewares/index')
+const {AuthRequestValidator} = require('../../middlewares/index')
 
 const router = express.Router();
 
 router.post('/signup',
+    AuthRequestValidator.validateUserAuth,
     UserController.create
 );
 
 router.post('/signin',
+    AuthRequestValidator.validateUserAuth,
     UserController.signIn
 );
 
